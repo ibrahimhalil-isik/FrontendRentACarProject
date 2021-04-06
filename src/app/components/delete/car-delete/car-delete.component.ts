8f0733b026b7f9644ff174be11390a3c8c9b0a04
qubra <qubra@DESKTOP-VEOETUV>
2021-04-06 08:17:36 +0300

    Kodlama.io

--- src/app/components/delete/car-delete/car-delete.component.ts
+++ src/app/components/delete/car-delete/car-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
   selector: 'app-car-delete',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDeleteComponent implements OnInit {
 
-  constructor() { }
+  carDeleteForm : FormGroup;
+
+  constructor(
+    private formBuilder:FormBuilder, 
+    private carService:CarService,
+    private toastrService:ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.createCarDeleteeForm();
   }
+
+  createCarDeleteeForm(){
+    this.carDeleteForm = this.formBuilder.group({
+      carId: ['',Validators.required],
+    })
+ }
+
+ delete(){
+  if(this.carDeleteForm.valid){
+    let carModel = Object.assign({},this.carDeleteForm.value)
+    this.carService.delete(carModel).subscribe(response=>{
+      this.toastrService.success(response.message,"Başarılı")
+    },responseError=>{
+      if(responseError.error.Errors.length>0){
+        for (let i = 0; i <responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+            ,"Doğrulama hatası")
+        }       
+      } 
+    })
+    
+  }else{
+    this.toastrService.error("Formunuz eksik","Dikkat")
+  }
+  
+}
 
 }
--- src/app/components/delete/color-delete/color-delete.component.ts
+++ src/app/components/delete/color-delete/color-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ColorService } from 'src/app/services/color/color.service';
 
 @Component({
   selector: 'app-color-delete',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorDeleteComponent implements OnInit {
 
-  constructor() { }
+  colorDeleteForm : FormGroup;
+
+  constructor(
+    private formBuilder:FormBuilder, 
+    private colorService:ColorService,
+    private toastrService:ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.createColorDeleteForm();
   }
 
+  createColorDeleteForm(){
+    this.colorDeleteForm = this.formBuilder.group({
+      colorId: ["",Validators.required],
+    })
+ }
+
+ delete(){
+  if(this.colorDeleteForm.valid){
+    let colorModel = Object.assign({},this.colorDeleteForm.value)
+    this.colorService.delete(colorModel).subscribe(response=>{
+      this.toastrService.success(response.message,"Başarılı")
+    },responseError=>{
+      if(responseError.error.Errors.length>0){
+        for (let i = 0; i <responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+            ,"Doğrulama hatası")
+        }       
+      } 
+    })
+    
+  }else{
+    this.toastrService.error("Formunuz eksik","Dikkat")
+  }
+  
+}
 }
